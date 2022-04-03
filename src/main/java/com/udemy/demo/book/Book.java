035b3344e34b0044240cdfec121e9b7bc1de5d3d
Sylvain Maestri <sylvainmaestri@gmail.com>
2022-04-03 22:16:55 +0200

    BookController

---
+++ src/main/java/com/udemy/demo/book/Book.java
@@ -0,0 +1,23 @@
+package com.udemy.demo.book;
+
+public class Book {
+
+    private String title;
+    private Category category;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+}
---
+++ src/main/java/com/udemy/demo/book/BookController.java
@@ -0,0 +1,40 @@
+package com.udemy.demo.book;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+
+@RestController
+public class BookController {
+
+    @GetMapping(value="/books")
+    public ResponseEntity list() {
+        Book book = new Book();
+        book.setTitle("title");
+        book.setCategory(new Category("BD"));
+        return new ResponseEntity<>(Arrays.asList(book), HttpStatus.OK);
+    }
+
+    @PostMapping(value="books")
+    public ResponseEntity create(Book book) {
+        // TODO
+        return new ResponseEntity(book, HttpStatus.CREATED);
+
+    }
+
+    @DeleteMapping(value="books/{bookId}")
+    public ResponseEntity deleteBook(@PathVariable("bookId") String bookId) {
+        // TODO
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+
+    }
+
+    @PutMapping(value="books/{bookId}")
+    public ResponseEntity updateBook(@PathVariable("bookId") String bookId, Book book) {
+        // TODO
+        return new ResponseEntity(HttpStatus.OK);
+
+    }
+}
