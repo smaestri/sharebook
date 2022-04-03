6bc7b10a7e8d4133c881bb734dad6787d31d4ad5
Sylvain Maestri <sylvainmaestri@gmail.com>
2022-04-03 22:16:55 +0200

    Session 15 - BookController

---
+++ src/main/java/com/udemy/demo/book/Category.java
@@ -0,0 +1,17 @@
+package com.udemy.demo.book;
+
+public class Category {
+    public Category(String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    private String label;
+}
