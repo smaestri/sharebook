5be61b17cbbe50fb6d026ee6937f77c16c1dea60
Sylvain Maestri <sylvainmaestri@gmail.com>
2022-04-05 21:45:06 +0200

    Session 19 - UserController

---
+++ src/main/java/com/udemy/demo/borrow/Borrow.java
@@ -0,0 +1,59 @@
+package com.udemy.demo.borrow;
+
+import com.udemy.demo.book.Book;
+import com.udemy.demo.user.User;
+
+import java.time.LocalDate;
+
+public class Borrow {
+
+    private User borrower;
+    private User lender;
+    private Book book;
+    private LocalDate askDate;
+
+    public LocalDate getAskDate() {
+        return askDate;
+    }
+
+    public void setAskDate(LocalDate askDate) {
+        this.askDate = askDate;
+    }
+
+    public LocalDate getCloseDate() {
+        return closeDate;
+    }
+
+    public void setCloseDate(LocalDate closeDate) {
+        this.closeDate = closeDate;
+    }
+
+    private LocalDate closeDate;
+
+
+    public User getBorrower() {
+        return borrower;
+    }
+
+    public void setBorrower(User borrower) {
+        this.borrower = borrower;
+    }
+
+    public User getLender() {
+        return lender;
+    }
+
+    public void setLender(User lender) {
+        this.lender = lender;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+
+}
