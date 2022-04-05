6e44538e9080a087754ebbcd4575623275977fc8
Sylvain Maestri <sylvainmaestri@gmail.com>
2022-04-05 22:04:18 +0200

    Session 24 - Validation

--- src/main/java/com/udemy/demo/user/User.java
+++ src/main/java/com/udemy/demo/user/User.java
@@ -1,9 +1,13 @@
 package com.udemy.demo.user;
 
+import javax.validation.constraints.Size;
+
 public class User {
 
     private String email;
+    @Size(min = 2, max = 25, message = "Firstname Entre 2 et 25 caracteres SVP")
     private String firstName;
+    @Size(min = 2, max = 25, message = "Lastname Entre 2 et 25 caracteres SVP")
     private String lastName;
     private String password;
 
--- src/main/java/com/udemy/demo/user/UserController.java
+++ src/main/java/com/udemy/demo/user/UserController.java
@@ -6,10 +6,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
 public class UserController {
     @PostMapping(value = "/users")
-    public ResponseEntity add(@RequestBody User user) {
+    public ResponseEntity add(@Valid @RequestBody User user) {
         User userOld = new User("toto@yopmail.com");
         return new ResponseEntity(userOld, HttpStatus.CREATED);
     }
