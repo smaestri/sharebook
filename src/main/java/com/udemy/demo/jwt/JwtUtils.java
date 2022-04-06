25265deb8c8563f9ea5bb65093d99e8a14d59c33
Sylvain Maestri <sylvainmaestri@gmail.com>
2022-04-06 22:27:56 +0200

    Session 69 - JwtUtils

--- src/main/java/com/udemy/demo/jwt/JwtUtils.java
+++ src/main/java/com/udemy/demo/jwt/JwtUtils.java
@@ -1,9 +1,50 @@
 package com.udemy.demo.jwt;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Component;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Component
 public class JwtUtils {
+
+    long JWT_VALIDITY = 5 * 60 * 60;
+
+    private static final String AUTHORITIES_KEY = "sub";
+
+    @Value("${jwt.secret}")
+    String secret;
+
     public String generateToken(Authentication authentication) {
-        return "";
+        Map<String, Object> claims = new HashMap<>();
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(authentication.getName())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + JWT_VALIDITY * 1000))
+                .signWith(SignatureAlgorithm.HS512, secret).compact();
+    }
+
+    public Authentication getAuthentication(String token) {
+        Claims claims = Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+
+        Collection<? extends GrantedAuthority> authorities = Arrays
+                .stream(claims.get(AUTHORITIES_KEY).toString().split(","))
+                .filter(auth -> !auth.trim().isEmpty())
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+
+        User principal = new User(claims.getSubject(), "", authorities);
+
+        return new UsernamePasswordAuthenticationToken(principal, token, authorities);
     }
 }
