15561f75f72b195b662660e7e224f8de7fcfb5f0
Sylvain Maestri <sylvainmaestri@gmail.com>
2022-04-06 22:17:39 +0200

    Set Spring security Configuration

---
+++ src/main/java/com/udemy/demo/configuration/SecurityConfiguration.java
@@ -0,0 +1,39 @@
+package com.udemy.demo.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    SecurityFilterChain web(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .exceptionHandling()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/users").permitAll()
+                        .requestMatchers("/authenticate").permitAll()
+                        .anyRequest().authenticated()
+
+                );
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
