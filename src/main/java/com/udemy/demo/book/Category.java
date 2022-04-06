ee943fd132a12a29340e57df8624f4cc06da01d1
Sylvain Maestri <sylvainmaestri@gmail.com>
2022-04-06 21:48:55 +0200

    Entity Category

--- src/main/java/com/udemy/demo/book/Category.java
+++ src/main/java/com/udemy/demo/book/Category.java
@@ -1,6 +1,29 @@
 package com.udemy.demo.book;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String label;
+
+    public Category() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public Category(String label) {
         this.label = label;
     }
@@ -12,6 +35,4 @@ public class Category {
     public void setLabel(String label) {
         this.label = label;
     }
-
-    private String label;
 }
