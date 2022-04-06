628949a99456de96cd47b269a1d1b9808abb83ca
Sylvain Maestri <sylvainmaestri@gmail.com>
2022-04-06 21:53:12 +0200

    User repository

---
+++ src/main/java/com/udemy/demo/user/UserRepository.java
@@ -0,0 +1,13 @@
+package com.udemy.demo.user;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+    List<User> findByEmail(String email);
+
+}
