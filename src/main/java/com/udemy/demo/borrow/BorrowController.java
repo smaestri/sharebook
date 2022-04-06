d5c733dbc6b1524a9c62098e213215f9e42da8a0
Sylvain Maestri <sylvainmaestri@gmail.com>
2022-04-06 22:04:54 +0200

    Branchement emprunts

--- src/main/java/com/udemy/demo/borrow/BorrowController.java
+++ src/main/java/com/udemy/demo/borrow/BorrowController.java
@@ -1,29 +1,77 @@
 package com.udemy.demo.borrow;
 
+import com.udemy.demo.book.Book;
+import com.udemy.demo.book.BookController;
+import com.udemy.demo.book.BookRepository;
+import com.udemy.demo.book.BookStatus;
+import com.udemy.demo.user.User;
+import com.udemy.demo.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class BorrowController {
 
-    @GetMapping(value = "/borrows")
+    @Autowired
+    BorrowRepository borrowRepository;
+
+    @Autowired
+    BookRepository bookRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping(value ="/borrows")
     public ResponseEntity getMyBorrows() {
-        // TODO
-        Borrow borrow = new Borrow();
-        borrow.setAskDate(LocalDate.now());
-        return new ResponseEntity(borrow, HttpStatus.OK);
+        List<Borrow> borrows = borrowRepository.findByBorrowerId(BookController.getUserConnectedId());
+        return new ResponseEntity(borrows, HttpStatus.OK);
     }
 
     @PostMapping("/borrows/{bookId}")
     public ResponseEntity createBorrow(@PathVariable("bookId") String bookId) {
-        return new ResponseEntity(HttpStatus.CREATED);
+        Integer userConnectedId = BookController.getUserConnectedId();
+        Optional<User> borrower = userRepository.findById(userConnectedId);
+        Optional<Book> book = bookRepository.findById(Integer.valueOf(bookId));
+
+        if(borrower.isPresent() && book.isPresent() && book.get().getStatus().equals(BookStatus.FREE)) {
+            Borrow borrow = new Borrow();
+            Book bookEntity = book.get();
+            borrow.setBook(bookEntity);
+            borrow.setBorrower(borrower.get());
+            borrow.setLender(bookEntity.getUser());
+            borrow.setAskDate(LocalDate.now());
+            borrowRepository.save(borrow);
+
+            bookEntity.setStatus(BookStatus.BORROWED);
+            bookRepository.save(bookEntity);
+            return new ResponseEntity(HttpStatus.CREATED);
+        }
+        return new ResponseEntity(HttpStatus.BAD_REQUEST);
     }
 
     @DeleteMapping("/borrows/{borrowId}")
     public ResponseEntity delete(@PathVariable("borrowId") String borrowId) {
+
+        Optional<Borrow> borrow = borrowRepository.findById(Integer.valueOf(borrowId));
+        if(borrow.isEmpty()) {
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        }
+
+        Borrow borrowEntity = borrow.get();
+        borrowEntity.setCloseDate(LocalDate.now());
+        borrowRepository.save(borrowEntity);
+
+        Book book = borrowEntity.getBook();
+        book.setStatus(BookStatus.FREE);
+        bookRepository.save(book);
+
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
+
 }
