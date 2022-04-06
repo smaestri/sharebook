c89207b89365f501f891db653d9fcf7e0c0479c2
Sylvain Maestri <sylvainmaestri@gmail.com>
2022-04-06 22:15:43 +0200

    UserDetailService

--- src/main/java/com/udemy/demo/user/UserRepository.java
+++ src/main/java/com/udemy/demo/user/UserRepository.java
@@ -3,11 +3,9 @@ package com.udemy.demo.user;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface UserRepository extends CrudRepository<UserInfo, Integer> {
 
-    List<UserInfo> findByEmail(String email);
+    UserInfo findOneByEmail(String email);
 
 }
