78a3bb2750d06c87160f2d6927e3a694bcb40ced
Sylvain Maestri <sylvainmaestri@gmail.com>
2022-04-06 21:46:02 +0200

    Finition Entity Book

--- src/main/java/com/udemy/demo/user/User.java
+++ src/main/java/com/udemy/demo/user/User.java
@@ -1,10 +1,10 @@
 package com.udemy.demo.user;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.validation.constraints.Size;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Size;
 
 @Entity
 public class User {
