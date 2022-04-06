561766b90e84d3520d81c8804ad15c59923ce852
Sylvain Maestri <sylvainmaestri@gmail.com>
2022-04-06 22:05:43 +0200

    Branchement Inscription

--- src/main/java/com/udemy/demo/user/UserController.java
+++ src/main/java/com/udemy/demo/user/UserController.java
@@ -1,5 +1,6 @@
 package com.udemy.demo.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,12 +8,22 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
+import java.util.List;
 
 @RestController
 public class UserController {
+
+    @Autowired
+    UserRepository userRepository;
+
     @PostMapping(value = "/users")
-    public ResponseEntity add(@Valid @RequestBody User user) {
-        User userOld = new User("toto@yopmail.com");
-        return new ResponseEntity(userOld, HttpStatus.CREATED);
+    public ResponseEntity add(@Valid @RequestBody  User user) {
+        List<User> users = userRepository.findByEmail(user.getEmail());
+        if(!users.isEmpty()) {
+            return new ResponseEntity("User already existing", HttpStatus.BAD_REQUEST);
+        }
+        userRepository.save(user);
+        return new ResponseEntity(user, HttpStatus.CREATED);
     }
+
 }
