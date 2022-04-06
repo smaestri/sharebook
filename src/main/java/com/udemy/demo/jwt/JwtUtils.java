6351e9ed062ee30ff0b7b50366552e36b5c1f764
Sylvain Maestri <sylvainmaestri@gmail.com>
2022-04-06 22:27:56 +0200

    JwtUtils

--- src/main/java/com/udemy/demo/jwt/JwtUtils.java
+++ src/main/java/com/udemy/demo/jwt/JwtUtils.java
@@ -1,9 +1,59 @@
 package com.udemy.demo.jwt;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtParser;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Component;
 
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Component
 public class JwtUtils {
+
+    long JWT_VALIDITY = 5 * 60 * 60;
+
+    private static final String AUTHORITIES_KEY = "sub";
+
+    private final Key key;
+
+    private final JwtParser jwtParser;
+
+    public JwtUtils( @Value("${jwt.secret}") String secret) {
+        this.key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+        this.jwtParser = Jwts.parserBuilder().setSigningKey(key).build();
+    }
+
     public String generateToken(Authentication authentication) {
-        return "";
+        Map<String, Object> claims = new HashMap<>();
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(authentication.getName())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + JWT_VALIDITY * 1000))
+                .signWith(key).compact();
+    }
+
+    public Authentication getAuthentication(String token) {
+        Claims claims = jwtParser.parseClaimsJws(token).getBody();
+
+        Collection<? extends GrantedAuthority> authorities = Arrays
+                .stream(claims.get(AUTHORITIES_KEY).toString().split(","))
+                .filter(auth -> !auth.trim().isEmpty())
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+
+        User principal = new User(claims.getSubject(), "", authorities);
+
+        return new UsernamePasswordAuthenticationToken(principal, token, authorities);
     }
 }
