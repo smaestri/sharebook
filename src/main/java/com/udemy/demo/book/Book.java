c845d59a561210a15c09ac08b2e6981b1126307c
Sylvain Maestri <sylvainmaestri@gmail.com>
2022-04-06 21:45:08 +0200

    Session 32 - Entity Book

--- src/main/java/com/udemy/demo/book/Book.java
+++ src/main/java/com/udemy/demo/book/Book.java
@@ -1,21 +1,61 @@
 package com.udemy.demo.book;
 
+import com.udemy.demo.user.User;
+
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 
+@Entity
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
 
     @NotBlank
     private String title;
+
+    @ManyToOne
     private Category category;
 
-    private BookStatus bookStatus;
+    @ManyToOne
+    private User user;
 
-    public BookStatus getBookStatus() {
-        return bookStatus;
+    private BookStatus status;
+
+    @Transient
+    private int categoryId;
+
+    public int getId() {
+        return id;
     }
 
-    public void setBookStatus(BookStatus bookStatus) {
-        this.bookStatus = bookStatus;
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public BookStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(BookStatus status) {
+        this.status = status;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
     }
 
     public String getTitle() {
