13da74b47505e6ba450325b5a7bc9491455960e6
Sylvain Maestri <sylvainmaestri@gmail.com>
2022-04-06 22:18:23 +0200

    Session 65 -  Suite Spring security Configuration

--- src/main/java/com/udemy/demo/configuration/SecurityConfiguration.java
+++ src/main/java/com/udemy/demo/configuration/SecurityConfiguration.java
@@ -1,12 +1,17 @@
 package com.udemy.demo.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
 
 import javax.servlet.http.Cookie;
@@ -14,6 +19,7 @@ import javax.servlet.http.Cookie;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
     @Autowired
     MyUserDetailService service;
 
@@ -43,5 +49,21 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                         response.addCookie(cookieToDelete);
                     }
                 });
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(service).passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    private PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
