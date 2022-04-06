6d83ce0355b7eb4e3e3722177434fc87a81b72f5
Sylvain Maestri <sylvainmaestri@gmail.com>
2022-04-06 22:26:32 +0200

    Session 67 - JwtController

---
+++ src/main/java/com/udemy/demo/jwt/JwtUtils.java
@@ -0,0 +1,9 @@
+package com.udemy.demo.jwt;
+
+import org.springframework.security.core.Authentication;
+
+public class JwtUtils {
+    public String generateToken(Authentication authentication) {
+        return "";
+    }
+}
