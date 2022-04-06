330a3bc4a5086584719a5a770b4b04f804d3c701
Sylvain Maestri <sylvainmaestri@gmail.com>
2022-04-06 21:46:57 +0200

    Entity User

--- src/main/java/com/udemy/demo/user/User.java
+++ src/main/java/com/udemy/demo/user/User.java
@@ -1,22 +1,41 @@
 package com.udemy.demo.user;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Size;
 
+@Entity
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
     private String email;
+
     @Size(min = 2, max = 25, message = "Firstname Entre 2 et 25 caracteres SVP")
-    private String firstName;
-    @Size(min = 2, max = 25, message = "Lastname Entre 2 et 25 caracteres SVP")
     private String lastName;
+
+    @Size(min = 2, max = 25, message = "Lastname Entre 2 et 25 caracteres SVP")
+    private String firstName;
+
     private String password;
 
-    public User(){
-
+    public User() {
     }
 
     public User(String email) {
         this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getLastName() {
