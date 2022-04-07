93bc77a4c5b21e607f6bb1fd97589d98329324e7
Sylvain Maestri <sylvainmaestri@gmail.com>
2022-04-07 21:59:07 +0200

    Exo props

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,47 +1,12 @@
-import logo from './logo.svg';
+import MyBooks from './MyBooks'
 import './App.css';
-import { Book } from './Book'
+
 
 function App() {
 
-
-  const renderBooks = () => {
-    const books = [
-      {
-        id: 1,
-        title: "asterix",
-        author: "Uderzo"
-      },
-      {
-        id: 2,
-        title: "tinitin",
-        author: "Herge"
-      }
-    ]
-    const resBooks = books.map(item => {
-      return <Book title={item.title} author={item.author} />
-    })
-
-    return resBooks;
-
-  }
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p style={{ color: "red" }}>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {renderBooks()}
-        </a>
-      </header>
+      <MyBooks />
     </div>
   );
 }
---
+++ frontend/my-app/src/MyBooks.js
@@ -0,0 +1,21 @@
+import Book from './Book'
+
+const MyBooks = () => {
+
+  const books = [
+    {
+      title: "asterix",
+      category: "BD",
+    },
+    {
+      title: "tintin",
+      category: "BD",
+    }
+  ]
+
+  {/*A vous d'implémenter!*/ }
+
+  return "TODO"
+}
+
+export default MyBooks
