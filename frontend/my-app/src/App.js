73ac1b7c1aa7fc7ac6994da4d784ba12170c3738
Sylvain Maestri <sylvainmaestri@gmail.com>
2022-04-07 22:12:56 +0200

    Session 97 - Routes

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,12 +1,27 @@
-import MyBooks from './MyBooks';
+import { Route, Routes } from 'react-router-dom'
+import AddBook from './AddBook';
+import AddUser from './AddUser';
+import ListBooks from './ListBooks'
+import MyBooks from './MyBooks'
+import Login from './Login'
+import MyBorrows from './MyBorrows'
 
 function App() {
 
   return (
-    <div className="App">
-      <MyBooks />
+    <div>
+      <div className="App">
+        <Routes>
+          <Route path="listBooks" element={<ListBooks />} />
+          <Route path="myBooks" element={<MyBooks />} />
+          <Route path="addBook" element={<AddBook />} />
+          <Route path="addBook/:bookId" element={<AddBook />} />
+          <Route path="myBorrows" element={<MyBorrows />} />
+          <Route path="login" element={<Login />} />
+          <Route path="addUser" element={<AddUser />} />
+        </Routes>
+      </div>
     </div>
   );
 }
-
 export default App;
