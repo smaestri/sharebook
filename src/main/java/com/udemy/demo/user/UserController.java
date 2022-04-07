436d4b3fbea1ab4a0821a8db5ab61861ad913561
Sylvain Maestri <sylvainmaestri@gmail.com>
2022-04-07 21:25:42 +0200

    Session 72 - Finalisation inscription

--- src/main/java/com/udemy/demo/user/UserController.java
+++ src/main/java/com/udemy/demo/user/UserController.java
@@ -1,12 +1,18 @@
 package com.udemy.demo.user;
 
+import com.udemy.demo.configuration.MyUserDetailService;
+import com.udemy.demo.jwt.JwtUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.util.List;
 
@@ -16,13 +22,23 @@ public class UserController {
     @Autowired
     UserRepository userRepository;
 
-    @PostMapping(value = "/users")
-    public ResponseEntity add(@Valid @RequestBody  User user) {
+    @Autowired
+    JwtUtils jwtUtils;
+
+    @PostMapping("/users")
+    public ResponseEntity add(@Valid @RequestBody  User user, HttpServletResponse response) {
+
         List<User> users = userRepository.findByEmail(user.getEmail());
         if(!users.isEmpty()) {
             return new ResponseEntity("User already existing", HttpStatus.BAD_REQUEST);
         }
+        user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
+        user.setLastName(StringUtils.capitalize(user.getLastName()));
+        user.setFirstName(StringUtils.capitalize(user.getFirstName()));
         userRepository.save(user);
+        String token = jwtUtils.generateToken(new MyUserDetailService.UserPrincipal(user));
+        Cookie cookie = new Cookie("token", token);
+        response.addCookie(cookie);
         return new ResponseEntity(user, HttpStatus.CREATED);
     }
 
