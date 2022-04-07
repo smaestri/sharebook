c8ba402de9ad3c8856c6e647d6f575f645897e6e
Sylvain Maestri <sylvainmaestri@gmail.com>
2022-04-07 22:19:45 +0200

    Session 101 - Styling Header

--- frontend/my-app/src/Header.js
+++ frontend/my-app/src/Header.js
@@ -1,9 +1,28 @@
 import { Link } from "react-router-dom";
 
 export default function Header() {
-    return <div>
-        <Link to="listBooks">Livres disponibles</Link>
-        <Link to="myBooks">Mes livres</Link>
-        <Link to="myBorrows">Mes emprunts</Link>
-        </div>
-} 
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/myBooks">Mes livres</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/myBorrows">Mes emprunts</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/listBooks">Livres disponibles</Link>
+                    </li>
+                </ul>
+                <div>Bienvenue, ...</div>
+                <button variant="secondary">Se déconnecter</button>
+            </div>
+        </nav>
+    )
+}
