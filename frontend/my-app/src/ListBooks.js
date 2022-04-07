73b10e340954a6cbdb81a7d88e57619ed52c7ff2
Sylvain Maestri <sylvainmaestri@gmail.com>
2022-04-07 22:04:52 +0200

    Session 93 - State

--- frontend/my-app/src/ListBooks.js
+++ frontend/my-app/src/ListBooks.js
@@ -3,28 +3,33 @@ import Book from './Book'
 
 export default class ListBooks extends React.Component {
 
+  constructor() {
+    super();
+    this.state = { books: [] }
+  }
   componentDidMount() {
-    // TODO charger les livres disponibles a partir des AP back
+    // TODO charger les livres disponibles a partir des API back
+    this.setState({
+      books: [
+        {
+          title: "asterix",
+          category: "BD",
+        },
+
+        {
+          title: "tintin",
+          category: "BD",
+        }
+      ]
+    })
   }
 
   render() {
 
-    const books= [
-      {
-        title: "asterix",
-        category: "BD",
-      },
-
-      {
-        title: "tintin",
-        category: "BD",
-      }
-    ]
-  
     return (<div>
       <h1>Livres diponibles</h1>
-      {books.length === 0 ? "Pas de livres disponibles" : null}
-      {books.map(book => (<div>
+      {this.state.books.length === 0 ? "Pas de livres disponibles" : null}
+      {this.state.books.map(book => (<div>
         <Book title={book.title} category={book.category}></Book>
         <button>Emprunter</button>
       </div>))}
