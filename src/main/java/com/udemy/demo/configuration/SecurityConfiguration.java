c99f5c7a866af208d93c401b9a5beda64ca4ae54
Sylvain Maestri <sylvainmaestri@gmail.com>
2022-04-07 21:23:54 +0200

    JwtFilter

--- src/main/java/com/udemy/demo/configuration/SecurityConfiguration.java
+++ src/main/java/com/udemy/demo/configuration/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.udemy.demo.configuration;
 
+import com.udemy.demo.jwt.JwtFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,12 +9,16 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 public class SecurityConfiguration {
 
     @Autowired
     RestAuthenticationEntryPoint restAuthenticationEntryPoint;
+
+    @Autowired
+    JwtFilter jwtFilter;
 
     @Bean
     SecurityFilterChain web(HttpSecurity http) throws Exception {
@@ -29,6 +34,7 @@ public class SecurityConfiguration {
                 .antMatchers("/users").permitAll()
                 .antMatchers("/authenticate").permitAll()
                 .anyRequest().authenticated();
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
