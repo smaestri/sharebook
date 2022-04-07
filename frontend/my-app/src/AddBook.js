d103ba7e20c0fc33ced7b4942cc67bc39b9b192f
Sylvain Maestri <sylvainmaestri@gmail.com>
2022-04-07 22:13:57 +0200

    Add Update book

--- frontend/my-app/src/AddBook.js
+++ frontend/my-app/src/AddBook.js
@@ -1,3 +1,9 @@
+import {useParams} from 'react-router-dom'
+
 export default function AddBook() {
+    let {bookId} = useParams();
+    if(bookId) {
+        return "update book" 
+    }
     return "addBook"
-} 
+}
