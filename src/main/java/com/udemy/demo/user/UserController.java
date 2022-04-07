f8de4d2309edffd67b3d73be4795d4517095da79
Sylvain Maestri <sylvainmaestri@gmail.com>
2022-04-07 21:25:42 +0200

    Finalisation inscription

--- src/main/java/com/udemy/demo/user/UserController.java
+++ src/main/java/com/udemy/demo/user/UserController.java
@@ -1,14 +1,20 @@
 package com.udemy.demo.user;
 
+import com.udemy.demo.jwt.JwtController;
+import com.udemy.demo.jwt.JwtFilter;
+import com.udemy.demo.jwt.JwtUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.util.List;
 
 @RestController
 public class UserController {
@@ -16,14 +22,35 @@ public class UserController {
     @Autowired
     UserRepository userRepository;
 
-    @PostMapping(value = "/users")
+    @Autowired
+    JwtUtils jwtUtils;
+
+    @Autowired
+    JwtController jwtController;
+
+    @PostMapping("/users")
     public ResponseEntity add(@Valid @RequestBody UserInfo userInfo) {
-        UserInfo user = userRepository.findOneByEmail(userInfo.getEmail());
-        if(user != null) {
+
+        UserInfo existingUser = userRepository.findOneByEmail(userInfo.getEmail());
+        if(existingUser != null) {
             return new ResponseEntity("User already existing", HttpStatus.BAD_REQUEST);
         }
-        UserInfo userSaved = userRepository.save(userInfo);
-        return new ResponseEntity(userSaved, HttpStatus.CREATED);
+        UserInfo user = saveUser(userInfo);
+        Authentication authentication = jwtController.logUser(userInfo.getEmail(), userInfo.getPassword());
+        String jwt = jwtUtils.generateToken(authentication);
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add(JwtFilter.AUTHORIZATION_HEADER, "Bearer " + jwt);
+        return new ResponseEntity<>(user, httpHeaders, HttpStatus.OK);
+    }
+
+    private UserInfo saveUser(UserInfo userInfo) {
+        UserInfo user = new UserInfo();
+        user.setEmail(userInfo.getEmail());
+        user.setPassword(new BCryptPasswordEncoder().encode(userInfo.getPassword()));
+        user.setLastName(StringUtils.capitalize(userInfo.getLastName()));
+        user.setFirstName(StringUtils.capitalize(userInfo.getFirstName()));
+        userRepository.save(user);
+        return user;
     }
 
 }
