a53538923bdb198699f79d1a7a20f7ac1ddb8d16
Sylvain Maestri <sylvainmaestri@gmail.com>
2022-04-08 22:42:57 +0200

    MyBorrows

--- frontend/my-app/src/MyBorrows.js
+++ frontend/my-app/src/MyBorrows.js
@@ -1,3 +1,52 @@
+import React from 'react'
+import Book from './Book'
+import './MyBorrows.scss'
+
 export default function MyBorrows() {
-    return "myborrows"
-} 
+
+    const [myBorrows, setMyBorrows] = React.useState([]);
+
+    React.useEffect(() => {
+
+        setMyBorrows([
+            {
+                lender: "toto",
+                borrower: "tata",
+                book: {
+                    name: "tintin",
+                    category: {
+                        label: "BD"
+                    }
+                }
+            }
+        ])
+    }, [])
+
+    const closeBorrow = (borrowId) => {
+    }
+
+    return (
+        <div className="container">
+            <h1>Mes emprunts</h1>
+            <div className="list-container">
+                {myBorrows.map((borrow) => {
+                    return (
+                        <div className="borrow-container">
+                            <Book
+                                title={borrow.book.title}
+                                category={borrow.book.category.label}
+                                lender={borrow.lender.firstName + " " + borrow.lender.lastName}
+                                askDate={borrow.askDate}
+                                closeDate={borrow.closeDate}>
+                            </Book>
+                            <div className="text-center">
+                                {borrow.closeDate ? "" : <button className="btn btn-primary btn-sm" onClick={() => closeBorrow(borrow.id)}>Clore</button>}
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
+            {myBorrows.length === 0 ? <div>Vous n'avez pas d'emprunt</div> : null}
+        </div>
+    )
+}
