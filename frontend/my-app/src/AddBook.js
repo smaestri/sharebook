dcd15a27a5e608a35f20b951c8761a6800c7e646
Sylvain Maestri <sylvainmaestri@gmail.com>
2022-04-08 22:32:17 +0200

    Session 108 - AddBook

--- frontend/my-app/src/AddBook.js
+++ frontend/my-app/src/AddBook.js
@@ -1,9 +1,41 @@
-import {useParams} from 'react-router-dom'
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
+import "./AddBook.scss"
 
 export default function AddBook() {
-    let {bookId} = useParams();
-    if(bookId) {
-        return "update book" 
+    let { bookId } = useParams();
+   
+    if (bookId) {
+        return "update book"
     }
-    return "addBook"
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        console.log("onSubmit")
+        // TODO
+    }
+
+    return (
+        <div className="container-add-book">
+            <h1>Ajouter un livre</h1>
+            <form onSubmit={onSubmit}>
+                <div>
+                    <label>Nom du livre</label>
+                    <input name="name" type="text" className="form-control"></input>
+                </div>
+                <div>
+                    <label>Catégorie du livre</label>
+                    <select name="categoryId" className="form-control">
+                        <option value="1">BD</option>
+                        <option value="2">Roman</option>
+                    </select>
+                </div>
+                <div className="container-submit">
+                    <input type="submit" value='Valider' className="btn btn-primary"></input>
+                </div>
+
+            </form>
+        </div>
+    )
 }
