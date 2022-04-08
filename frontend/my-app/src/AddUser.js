2c1bd5a3675cbb125dacda3d59acf03b2537450c
Sylvain Maestri <sylvainmaestri@gmail.com>
2022-04-08 22:41:02 +0200

    Inscription

--- frontend/my-app/src/AddUser.js
+++ frontend/my-app/src/AddUser.js
@@ -1,3 +1,60 @@
-export default function AddUser() {
-    return "addUser"
-} 
+import React from 'react'
+import { Link } from "react-router-dom";
+
+import './AddUser.scss'
+
+class AddUser extends React.Component {
+
+  constructor() {
+    super();
+    this.state = { userData: {}}
+  }
+
+  handleChange = (event) => {
+    let currentState = { ...this.state.userData };
+    currentState[event.target.name] = event.target.value;
+    this.setState({ userData: currentState })
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    console.log("onSubmit")
+    console.log(this.state.userData)
+  }
+
+  render() {
+    return (
+      <div className="add-user-container">
+        <div>
+          <h1>M'inscrire</h1>
+          <div>
+            <form onSubmit={this.onSubmit}>
+              <div>
+                <label>email</label>
+                <input name="email" type="text" className="form-control" onChange={this.handleChange} />
+              </div>
+              <div>
+                <label>nom</label>
+                <input name="lastName" type="text" className="form-control" onChange={this.handleChange} />
+              </div>
+              <div>
+                <label>prenom</label>
+                <input name="firstName" type="text" className="form-control" onChange={this.handleChange} />
+              </div>
+              <div>
+                <label>password</label>
+                <input name="password" type="password" className="form-control" onChange={this.handleChange} />
+              </div>
+              <div className="container-valid text-center">
+                <input type="submit" value="Valider" className="btn btn-primary" onChange={this.handleChange} />
+              </div>
+            </form>
+          </div>
+          <div><Link to="/">Retour à l'accueil</Link></div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default AddUser
