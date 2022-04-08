a7a56d80a6e3ba88fe1d678d63d8cde46d716d2a
Sylvain Maestri <sylvainmaestri@gmail.com>
2022-04-08 22:43:53 +0200

    MyBooks en fonction

--- frontend/my-app/src/MyBooks.js
+++ frontend/my-app/src/MyBooks.js
@@ -1,51 +1,45 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import Book from './Book'
 
 import './MyBooks.scss'
 
-class MyBooks extends React.Component {
+const MyBooks = () => {
 
-  constructor() {
-    super();
-    this.state = { books: [] }
-  }
-  componentDidMount() {
+  const [myBooks, setMyBooks] = React.useState([])
+  React.useEffect(() => {
     // TODO charger mes livres
-    this.setState({
-      books: [
-        {
-          title: "asterix",
-          category: "BD",
-        },
+    setMyBooks(
+        [
+          {
+            title: "asterix",
+            category: "BD",
+          },
 
-        {
-          title: "tintin",
-          category: "BD",
-        }
-      ]
-    })
-  }
+          {
+            title: "tintin",
+            category: "BD",
+          }
+        ]
+    )
+  }, [])
 
-  render() {
-    return (
-      <div className="container">
-        <h1>Mes livres</h1>
-        <div className="list-container">
-          {this.state.books.length === 0 ? "Vous n'avez pas déclaré de livres" : null}
-          {this.state.books.map((book, key) => (<div key={key} className="mybook-container">
-            <Book title={book.title} category={book.category}></Book>
-            <div className="container-buttons">
-              <Link to={`/addBook/${book.id}`}>
-                <button className="btn btn-primary btn-sm">Modifier</button>
-              </Link>
-              <button className="btn btn-primary btn-sm">Supprimer</button>
-            </div>
-          </div>))}
-        </div>
-        <Link to="/addBook"><button className="btn btn-primary btn-sm">Nouveau livre</button></Link>
-      </div>)
-  }
-
+  return (
+    <div className="container">
+      <h1>Mes livres</h1>
+      <div className="list-container">
+        {myBooks.length === 0 ? "Vous n'avez pas déclaré de livres" : null}
+        {myBooks.map((book) => (<div className="mybook-container">
+          <Book title={book.title} category={book.category}></Book>
+          <div className="container-buttons">
+            <Link to={`/addBook/${book.id}`}>
+              <button className="btn btn-primary btn-sm">Modifier</button>
+            </Link>
+            <button className="btn btn-primary btn-sm">Supprimer</button>
+          </div>
+        </div>))}
+      </div>
+      <Link to="/addBook"><button className="btn btn-primary btn-sm">Nouveau livre</button></Link>
+    </div>)
 }
-export default MyBooks
+export default MyBooks;
