6dd415db0994a78f13b94ccff20097bb49611ca5
Sylvain Maestri <sylvainmaestri@gmail.com>
2022-04-08 22:36:49 +0200

    Login

--- frontend/my-app/src/Login.js
+++ frontend/my-app/src/Login.js
@@ -1,3 +1,45 @@
-export default function Login() {
-    return "Login"
-} 
+import React from 'react'
+import logo from './logo.jpg';
+import { Link } from "react-router-dom";
+import './Login.scss'
+
+export default class Login extends React.Component {
+
+    constructor() {
+        super();
+        this.state = { userData: {} }
+        this.onSubmit = this.onSubmit.bind(this)
+    }
+
+    onSubmit(event) {
+        event.preventDefault();
+        console.log("onsubmit")
+        console.log(this.state.userData)
+    }
+    render() {
+        return (
+            <div className="login-container">
+                <div>
+                    <div>
+                        <img src={logo} alt="Logo" />
+                    </div>
+                    <div className="title">
+                        Bienvenue sur Sharebook!
+                    </div>
+                    <div className="form-container">
+                        <form onSubmit={this.onSubmit}>
+                            <span>Mail: </span>
+                            <input type="text" className="form-control" name="email" onChange={this.handleChange}></input>
+                            <span>Passsword: </span>
+                            <input type="password" className="form-control" name="password" onChange={this.handleChange}></input>
+                            <div>
+                                <input type="submit" className="btn btn-primary" value="OK" />
+                            </div>
+                        </form>
+                    </div>
+                    <div><Link to="/addUser">M'inscrire</Link></div>
+                </div>
+            </div>
+        )
+    }
+}
