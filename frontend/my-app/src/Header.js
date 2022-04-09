76ccacf85891f696f7a0fbb32e0783d59fb1b74c
Sylvain Maestri <sylvainmaestri@gmail.com>
2022-04-09 15:15:52 +0200

    Session 147 - Header responsive

--- frontend/my-app/src/Header.js
+++ frontend/my-app/src/Header.js
@@ -1,6 +1,8 @@
 import { Link, useNavigate } from "react-router-dom";
 import axios from 'axios';
 
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 export default function Header({ userInfo, setUserInfo }) {
     const history = useNavigate();
     const signout = () => {
