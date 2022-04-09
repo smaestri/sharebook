61f16d0984109ca6b37b492c8dc93a77947fa54f
Sylvain Maestri <sylvainmaestri@gmail.com>
2022-04-09 14:59:40 +0200

    Branchement emprunter

--- frontend/my-app/src/ListBooks.js
+++ frontend/my-app/src/ListBooks.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Book from './Book'
 import axios from 'axios';
+import { useNavigate } from "react-router-dom";
 
 import './MyBooks.scss'
 import './ListBooks.scss'
 
-export default class ListBooks extends React.Component {
+class ListBooks extends React.Component {
 
   constructor() {
     super();
@@ -20,21 +21,32 @@ export default class ListBooks extends React.Component {
       
    }
 
+   borrowBook(bookId) {
+    axios.post(`/borrows/${bookId}`, {}).then(()=> {
+      this.props.history('/myBorrows')
+    })
+  }
+
   render() {
 
-    return (<div className="container" >
+    return (<div className="container"  >
       <h1>Livres diponibles</h1>
       <div className="list-container">
         {this.state.books.length === 0 ? "Pas de livres disponibles" : null}
         {this.state.books.map((book, key) => (<div key={key} className="list-book-container">
-          <Book title={book.title} category={book.category.label}></Book>
+          <Book title={book.title} category={book.category.label} lender={`${book.user.firstName} ${book.user.lastName}`}></Book>
           <div className="text-center">
-            <button className="btn btn-primary btn-sm">Emprunter</button>
+            <button className="btn btn-primary btn-sm" onClick={() => this.borrowBook(book.id)}>Emprunter</button>
           </div>
         </div>))}
       </div>
     </div>)
 
   }
+}
 
+// Wrap and export
+export default function (props) {
+  const history = useNavigate();
+  return <ListBooks {...props} history={history} />;
 }
