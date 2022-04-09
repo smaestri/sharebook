c1129e10690d1378f7d8cf3e4888802c4509da84
Sylvain Maestri <sylvainmaestri@gmail.com>
2022-04-09 15:12:29 +0200

    Session 145 - Modal user existant

--- frontend/my-app/src/AddUser.js
+++ frontend/my-app/src/AddUser.js
@@ -1,20 +1,24 @@
 import React from 'react'
 import { Link, useNavigate } from "react-router-dom";
 import axios from 'axios';
-
+import SimpleModal from './SimpleModal'
 import './AddUser.scss'
 
 class AddUser extends React.Component {
 
   constructor() {
     super();
-    this.state = { userData: {}}
+    this.state = { userData: {}, showModal: false }
   }
 
   handleChange = (event) => {
     let currentState = { ...this.state.userData };
     currentState[event.target.name] = event.target.value;
     this.setState({ userData: currentState })
+  }
+
+  handleCloseModal = () => {
+    this.setState({ showModal: false })
   }
 
   onSubmit = (event) => {
@@ -24,40 +28,50 @@ class AddUser extends React.Component {
     }).then(response => {
       this.props.setUserInfo(response.data.firstName + " " + response.data.lastName)
       this.props.history("/myBooks")
+    }).catch(() => {
+      this.setState({ showModal: true })
     })
   }
 
   render() {
     return (
-      <div className="add-user-container">
-        <div>
-          <h1>M'inscrire</h1>
+      <>
+        <div className="add-user-container">
           <div>
-            <form onSubmit={this.onSubmit}>
-              <div>
-                <label>email</label>
-                <input name="email" type="text" className="form-control" onChange={this.handleChange} />
-              </div>
-              <div>
-                <label>nom</label>
-                <input name="lastName" type="text" className="form-control" onChange={this.handleChange} />
-              </div>
-              <div>
-                <label>prenom</label>
-                <input name="firstName" type="text" className="form-control" onChange={this.handleChange} />
-              </div>
-              <div>
-                <label>password</label>
-                <input name="password" type="password" className="form-control" onChange={this.handleChange} />
-              </div>
-              <div className="container-valid text-center">
-                <input type="submit" value="Valider" className="btn btn-primary" onChange={this.handleChange} />
-              </div>
-            </form>
+            <h1>M'inscrire</h1>
+            <div>
+              <form onSubmit={this.onSubmit}>
+                <div>
+                  <label>email</label>
+                  <input name="email" type="text" className="form-control" onChange={this.handleChange} />
+                </div>
+                <div>
+                  <label>nom</label>
+                  <input name="lastName" type="text" className="form-control" onChange={this.handleChange} />
+                </div>
+                <div>
+                  <label>prenom</label>
+                  <input name="firstName" type="text" className="form-control" onChange={this.handleChange} />
+                </div>
+                <div>
+                  <label>password</label>
+                  <input name="password" type="password" className="form-control" onChange={this.handleChange} />
+                </div>
+                <div className="container-valid text-center">
+                  <input type="submit" value="Valider" className="btn btn-primary" onChange={this.handleChange} />
+                </div>
+              </form>
+            </div>
+            <div><Link to="/">Retour à l'accueil</Link></div>
           </div>
-          <div><Link to="/">Retour à l'accueil</Link></div>
         </div>
-      </div>
+        <SimpleModal
+          title={"Mail déja utilisé"}
+          bodyTxt={"Cet email est déja utilisé, merci d'en saisir un autre"}
+          handleCloseModal={this.handleCloseModal}
+          showModal={this.state.showModal}
+        ></SimpleModal>
+      </>
     )
   }
 }
