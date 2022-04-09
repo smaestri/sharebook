9df4ad312f3eabf242d1f13b0c8018ac4c6b23bd
Sylvain Maestri <sylvainmaestri@gmail.com>
2022-04-09 14:49:48 +0200

    Branchement Header

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -8,12 +8,13 @@ import MyBooks from './MyBooks'
 import Login from './Login'
 import Header from './Header'
 import MyBorrows from './MyBorrows'
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export const AUTH_TOKEN_KEY = 'jhi-authenticationToken';
 
 function App() {
+
+  const [userInfo, setUserInfo] = React.useState('');
 
   useEffect(() => {
     axios.interceptors.request.use(function (request) {
@@ -29,19 +30,19 @@ function App() {
 
   return (
     <div>
-    <Header />
-   <div className="App">
-     <Routes>
-       <Route path="listBooks" element={<ListBooks />} />
-       <Route path="myBooks" element={<MyBooks />} />
-       <Route path="addBook" element={<AddBook />} />
-       <Route path="addBook/:bookId" element={<AddBook />} />
-       <Route path="myBorrows" element={<MyBorrows />} />
-       <Route path="login" element={<Login />} />
-       <Route path="addUser" element={<AddUser />} />
-     </Routes>
-   </div>
- </div>
+      <Header userInfo={userInfo}/>
+      <div className="App">
+        <Routes>
+          <Route path="listBooks" element={<ListBooks />} />
+          <Route path="myBooks" element={<MyBooks />} />
+          <Route path="addBook" element={<AddBook />} />
+          <Route path="addBook/:bookId" element={<AddBook />} />
+          <Route path="myBorrows" element={<MyBorrows />} />
+          <Route path="login" element={<Login />} />
+          <Route path="addUser" element={<AddUser  setUserInfo={setUserInfo} />} />
+        </Routes>
+      </div>
+    </div>
   );
 }
 export default App;
