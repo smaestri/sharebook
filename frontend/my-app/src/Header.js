5c8fba0d7af28e215f98a4ab8b139ff2a94fe5e1
Sylvain Maestri <sylvainmaestri@gmail.com>
2022-04-09 15:14:16 +0200

    Session 146 - Bootstrap5

--- frontend/my-app/src/Header.js
+++ frontend/my-app/src/Header.js
@@ -12,26 +12,23 @@ export default function Header({ userInfo, setUserInfo }) {
 
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        <div className="container-fluid">
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
-
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav mr-auto">
-                    <li className="nav-item">
+                <div className="navbar-nav mr-auto me-auto">
                         <Link className="nav-link" to="/myBooks">Mes livres</Link>
-                    </li>
-                    <li className="nav-item">
                         <Link className="nav-link" to="/myBorrows">Mes emprunts</Link>
-                    </li>
-                    <li className="nav-item">
                         <Link className="nav-link" to="/listBooks">Livres disponibles</Link>
-                    </li>
-                </ul>
-                <div>Bienvenue, {userInfo}</div>
-                <button variant="secondary" onClick={signout}>Se déconnecter</button>
+                </div>
+                <div className="navbar-nav d-flex align-items-center">
+                    <div className="me-2">Bienvenue, {userInfo}</div>
+                    <button type="button" variant="secondary" className="btn btn-secondary" onClick={signout}>Se déconnecter</button>
+                </div>
             </div>
-        </nav>
+        </div>
+    </nav>
     )
 }
