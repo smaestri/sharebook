cad59fb162d63476b36be36c6fd815078dec8bdc
Sylvain Maestri <sylvainmaestri@gmail.com>
2022-04-09 15:00:43 +0200

    Mes emprunts

--- frontend/my-app/src/MyBorrows.js
+++ frontend/my-app/src/MyBorrows.js
@@ -1,28 +1,25 @@
 import React from 'react'
 import Book from './Book'
+import axios from 'axios';
 import './MyBorrows.scss'
 
 export default function MyBorrows() {
 
     const [myBorrows, setMyBorrows] = React.useState([]);
+    const getMyBorrows = () => {
+        axios.get('/borrows').then(response => {
+            setMyBorrows(response.data)
+           })
+    }
 
-    React.useEffect(() => {
-
-        setMyBorrows([
-            {
-                lender: "toto",
-                borrower: "tata",
-                book: {
-                    name: "tintin",
-                    category: {
-                        label: "BD"
-                    }
-                }
-            }
-        ])
-    }, [])
+    React.useEffect(()=> {
+        getMyBorrows();
+    },[])
 
     const closeBorrow = (borrowId) => {
+        axios.delete(`/borrows/${borrowId}`).then(response => {
+            getMyBorrows();
+           })
     }
 
     return (
