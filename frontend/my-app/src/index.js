bd295327cfa3e1db2e02b3f321b68b2f0fb9fa77
Sylvain Maestri <sylvainmaestri@gmail.com>
2022-04-09 14:37:13 +0200

    Session 119 - Connexion AddUser

--- frontend/my-app/src/index.js
+++ frontend/my-app/src/index.js
@@ -7,8 +7,8 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-     <BrowserRouter>
-       <App />
+    <BrowserRouter>
+      <App />
      </BrowserRouter>
   </React.StrictMode>
 );
