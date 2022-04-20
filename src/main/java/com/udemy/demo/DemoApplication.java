f4a64723d0252ed274bc5b5d79a4d9dcff81fb20
Sylvain Maestri <sylvainmaestri@gmail.com>
2022-04-20 22:23:32 +0200

    Profiles Spring

--- src/main/java/com/udemy/demo/DemoApplication.java
+++ src/main/java/com/udemy/demo/DemoApplication.java
@@ -1,13 +1,21 @@
 package com.udemy.demo;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication  implements CommandLineRunner {
 
+	@Value("${jwt.secret}")
+	private String jwt;
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("property jwt value is: " + jwt);
+	}
 }
