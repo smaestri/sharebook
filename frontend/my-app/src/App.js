bde5281e27d9887862c44eae7d4396d85a10614b
Sylvain Maestri <sylvainmaestri@gmail.com>
2022-04-22 18:26:45 +0200

    Session 157 - Fix warnings

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Routes, useNavigate, useLocation, BrowserRouter } from 'react-router-dom'
+import { Route, Routes, useNavigate, useLocation } from 'react-router-dom'
 import axios from 'axios';
 import AddBook from './AddBook';
 import AddUser from './AddUser';
