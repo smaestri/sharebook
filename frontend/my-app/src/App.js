ac44ba24c7f688fbd5d9dab7c6e5ffbf5cf3c0a4
Sylvain Maestri <sylvainmaestri@gmail.com>
2022-04-22 18:26:45 +0200

    Fix warnings

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react'
-import { Route, Routes, useNavigate, useLocation, BrowserRouter } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Route, Routes, useNavigate, useLocation } from 'react-router-dom'
 import axios from 'axios';
 import AddBook from './AddBook';
 import AddUser from './AddUser';
