0aec64a4e8524ccf8fde2514b94a9751364d97f5
Sylvain Maestri <sylvainmaestri@gmail.com>
2022-07-08 20:24:04 +0200

    Session 74 - isConnected

--- src/main/java/com/udemy/demo/user/UserController.java
+++ src/main/java/com/udemy/demo/user/UserController.java
@@ -8,8 +8,11 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,6 +44,15 @@ public class UserController {
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.add(JwtFilter.AUTHORIZATION_HEADER, "Bearer " + jwt);
         return new ResponseEntity<>(user, httpHeaders, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/isConnected")
+    public ResponseEntity getUSerConnected() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (principal instanceof UserDetails) {
+            return new ResponseEntity(((UserDetails) principal).getUsername(), HttpStatus.OK);
+        }
+        return new ResponseEntity("User is not connected", HttpStatus.FORBIDDEN);
     }
 
     private UserInfo saveUser(UserInfo userInfo) {
